daf020538f4be4b3c210d458a04db2a3d6ec2fff
Grant Skaggs <grant.skaggs@outlook.com>
2022-04-12 08:31:15 -0500

    Edit executeables

--- python/gierer-mienhardt.sh
+++ python/gierer-mienhardt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 run_simulation.py --param_search \
--t 1 -T 3000 \
+-t 6 -T 3000 \
 -p_a .5 .5 1 \
 -p_a0 0 0 1 \
 -u_a .5 .5 1 \
--- python/param_search_gm.sh
+++ python/param_search_gm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 python3 run_simulation.py --param_search \
--t 1 -T 3000 \
--p_a 0 1 10 \
+-t 6 -T 3000 \
+-p_a 0 1 5 \
 -p_a0 0 0 1 \
--u_a 0 .5 3 \
--p_h 0 1 10 \
+-u_a 0 .5 2 \
+-p_h 0 1 5 \
 -p_h0 0 0 1 \
--u_h 0 .5 3 \
+-u_h 0 .5 2 \
 -kappa 0 .54 2 \
 --dirichlet_vis
