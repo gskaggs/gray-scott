ed5f167d059c6f61c1ecebfaa032f951dcc73db1
Grant Skaggs <grant.skaggs@outlook.com>
2022-04-14 09:08:38 -0500

    Update ga script for gierer mienhardt

--- python/ga_dirichlet_gm.sh
+++ python/ga_dirichlet_gm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 run_simulation.py --genetic_algorithm -fitness dirichlet -t 6 -T 2000 -num_iters 30 \
+python3 run_simulation.py --genetic_algorithm -fitness dirichlet -t 6 -T 2000 -num_iters 5 \
 -rd gierer_mienhardt \
 -rho .5 \
 -mu 1 \
 -nu .9 \
--kappa .1 .8 6 \
+-kappa .1 .8 30 \
 --dirichlet_vis
