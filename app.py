cde9c833b1909df473f79b7741a7e32970f58e6d
Grant Skaggs <grant.skaggs@outlook.com>
2022-04-25 20:54:29 -0500

    Temp edit

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.title("Genetic Algorithms for the Physical Simulation of Flower Pigmentation")
 
-st.markdown("An undergraduate thesis by _Grant Skaggs._")
+st.markdown(
+    '''
+    An undergraduate thesis by _Grant Skaggs._
+
+    [https://open.spotify.com/track/6tuxI8MMx9T67pYjlMvgxG?si=90123bffd5d14d8a](https://open.spotify.com/track/6tuxI8MMx9T67pYjlMvgxG?si=90123bffd5d14d8a)
+    ''')
