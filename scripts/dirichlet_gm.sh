c2e36d920bbf412498c6e81c9f4c6bc06342cac3
Grant Skaggs <grant.skaggs@outlook.com>
2022-04-29 01:54:17 -0500

    Update script

--- scripts/dirichlet_gm.sh
+++ scripts/dirichlet_gm.sh
@@ -3,6 +3,6 @@
 python3 ./sim_driver_tui.py --genetic_algorithm \
  -fitness dirichlet  \
  -num_iters 20 \
- -T 2000 \
+ -T 100 \
  -num_individuals 30 \
  -rd gierer_mienhardt
--- scripts/isolate_gm.sh
+++ scripts/isolate_gm.sh
@@ -3,7 +3,4 @@
 python3 ./sim_driver_tui.py --param_search \
 -rd gierer_mienhardt \
 -t 6 -T 50 \
--rho .5 \
--mu 1 \
--nu .9 \
--kappa .238 
+-num_individuals 1
