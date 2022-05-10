cfacc1802b7ea6d4847be6e593c0e76b7b959355
Grant Skaggs <grant.skaggs@outlook.com>
2022-05-10 08:41:37 -0500

    Remove extraneus file

--- temp.py
+++
@@ -1,9 +0,0 @@
-from python.simulator import ReactionDiffusionSimulator
-from python.ga import Chromosome
-
-c = Chromosome({}, 0, 0)
-sim = ReactionDiffusionSimulator(chromosome=c)
-
-
-_, _, image = sim.integrate(0)
-image.show()
